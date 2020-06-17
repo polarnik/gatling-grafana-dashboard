c9b453b5403d90f88d8ff7b57e8875a79cf97ae4
polarnik <owasp@yandex.ru>
2020-06-17 15:46:29 +0300

    fix

--- devenv/devenv.run.influxdb.sh
+++ devenv/devenv.run.influxdb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")">/dev/null 2>&1 && pwd)"
 
 docker network create loadlab
 
